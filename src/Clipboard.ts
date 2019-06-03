7ed4a96b0c419eee9b74f616f2be00528e3cf80e
h2ck4u <h2ckforu@gmail.com>
2019-06-03 22:09:52 +0900

    :fire: cloneContents처리.

--- src/Clipboard.ts
+++ src/Clipboard.ts
@@ -27,7 +27,7 @@ class Clipboard {
         console.log('copy event is called!');
         const text = window.getSelection().toString();
         e.clipboardData.setData('text/plain', text);
-        // e.clipboardData.setData('text/html', this.__getSelectedElemenmts__());
+        e.clipboardData.setData('text/html', this.__getSelectedElemenmts__());
         this.elClipboard.innerText = text;
         console.log(this.elClipboard.innerText);
 
@@ -96,7 +96,7 @@ class Clipboard {
         const elClipboard = document.createElement('div');
         elClipboard.contentEditable = 'true';
         elClipboard.id = 'clipboard';
-        elClipboard.style.cssText = "width: 200px; height: 100px; border: solid 1px orange";
+        elClipboard.style.cssText = "float:left; width: 200px; height: 100px; border: solid 1px orange";
         document.body.appendChild(elClipboard);
 
         this.elClipboard = elClipboard;
@@ -128,54 +128,9 @@ class Clipboard {
     }
 
     __getSelectedElemenmts__() {
-        const rootNode = this.targetElement;
-        const clonedRoot = this.targetElement.cloneNode(true);
         const sel = window.getSelection();
-        const anchorNode = sel.anchorNode;
-        const anchorOffset = sel.anchorOffset;
-
-        const focusNode = sel.focusNode;
-        const focusOffset = sel.focusOffset;
-
-        let anchorChild = anchorNode;
-        while (anchorChild != rootNode.firstChild) {
-            anchorChild = anchorChild.parentElement;
-        }
-        let focusChild = focusNode;
-        while (focusChild != rootNode.lastChild) {
-            focusChild = focusChild.parentElement;
-        }
-
-        const clonedAnchorChild = anchorChild.cloneNode(true);
-        const clonedFocusChild = focusChild.cloneNode(true);
-
-        const tmp = document.createElement('div');
-        tmp.id = 'tmp';
-
-        let isContains = false;
-        for (let i = 0; i < rootNode.childElementCount; i++) {
-            let currNode = rootNode.children[i];
-            if (currNode.isEqualNode(clonedAnchorChild)) {
-                clonedAnchorChild.textContent = clonedAnchorChild.textContent.slice(anchorOffset);
-                tmp.append(clonedAnchorChild);
-                isContains = true;
-            }
-
-            if (currNode.isEqualNode(clonedFocusChild)) {
-                clonedFocusChild.textContent = clonedFocusChild.textContent.slice(0, focusOffset);
-                tmp.append(clonedFocusChild);
-                isContains = false;
-            }
-            if (isContains && !currNode.isEqualNode(clonedAnchorChild)) {
-                tmp.append(currNode.cloneNode(true));
-            }
-        }
-
-        console.log(tmp.innerText);
-
-        // clonedAnchorChild.textContent = clonedAnchorChild.textContent.slice(anchorOffset);
-        // clonedFocusChild.textContent = clonedFocusChild.textContent.slice(0, focusOffset);
-        // console.log(clonedAnchorChild);
+        const range = sel.getRangeAt(0);
+        return range.cloneContents();
     }
 }
 
